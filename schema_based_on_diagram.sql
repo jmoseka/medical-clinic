a34d24b2330025aefc9945f464a74a3e4e32d5b7
Brian Nyachae <techconsultants.bree@gmail.com>
2022-07-13 12:28:59 +0300

    add invoice and invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,3 +18,26 @@ CREATE TABLE treatments (
     name varchar(100)
 );
 
+CREATE TABLE invoices (
+    id BIGSERIAL PRIMARY KEY,
+    total_amount dec,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id int,
+
+    CONSTRAINT fk_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+    id BIGSERIAL PRIMARY KEY,
+    unit_price DEC,
+    quantity INT,
+    total_price DEC,
+    invoice_id INT,
+    treatment_id INT,
+
+    CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
+
