86292b59f21bc8fd02a331f0dc8bb56e4ed10d6d
Hammed Adisa <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-13 15:05:22 +0100

    Added medical histories treatment_id index

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -45,4 +45,4 @@ CREATE INDEX ON invoices (medical_history_id);
 CREATE INDEX ON invoice_items (invoice_id);
 CREATE INDEX ON invoice_items (treatment_id);
 CREATE INDEX ON medical_histories_has_treatments (medical_history_id);
-
+CREATE INDEX ON medical_histories_has_treatments (treatment_id);
